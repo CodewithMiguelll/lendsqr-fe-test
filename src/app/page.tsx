43f8f9f4739f7d43713d2aac94f0a877171b82f8
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2026-01-26 21:22:41 +0100

    feat: enhance sidebar with dashboard and customer navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import { Briefcase, ChevronDown } from "lucide-react";
+import { Briefcase, ChevronDown, House, Users } from "lucide-react";
 import { Work_Sans } from "next/font/google";
 import Link from "next/link";
 
@@ -17,6 +17,71 @@ export default function Home() {
             <Briefcase size={24} />
             <span>Switch Organization</span>
             <ChevronDown size={16} />
+          </div>
+          <div className={styles.dashboard}>
+            <House size={24} /> <span>Dashboard</span>
+          </div>
+          <div className={styles.sidebarMenu}>
+            <h4>Customers</h4>
+            <ul>
+              <li>
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Users size={16} />
+                  </span>{" "}
+                  Users
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Guarantors
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Loans
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Decision Models
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Savings
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Loan Requests
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Whitelist
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <span></span>
+                <Link className={styles.customersNav} href="#">
+                  Karma
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
         <div className={styles.content}></div>

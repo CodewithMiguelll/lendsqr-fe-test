a14a829ea6013248dfcf49bce20ca6fdeba919af
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2026-01-26 20:28:22 +0100

    feat: restructure sidebar component and update styles for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { Briefcase, ChevronDown } from "lucide-react";
+import { Work_Sans } from "next/font/google";
+import Link from "next/link";
 
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+})
 export default function Home() {
   return (
     <>
-    <div className={styles.page}>
-      <h1 className={styles.title}>Welcome to Lendsqr</h1>
-    </div>
+      <div className={`${workSans.className} ${styles.page}`}>
+        <div className={styles.sidebar}>
+          <div className={styles.sidebarHeader}>
+            <Briefcase size={24} />
+            <span>Switch Organization</span>
+            <ChevronDown size={16} />
+          </div>
+        </div>
+        <div className={styles.content}></div>
+      </div>
     </>
   );
 }

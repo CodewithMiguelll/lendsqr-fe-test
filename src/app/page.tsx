691dca49bd8631716ae136bca0a4d4b9f5f71f49
Miguel <113023936+CodewithMiguelll@users.noreply.github.com>
2026-01-27 13:52:59 +0100

    feat: update sidebar with new navigation items and improve styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import { Briefcase, ChevronDown, House, Users } from "lucide-react";
+import {
+  Briefcase,
+  ChevronDown,
+  House,
+  HandshakeIcon,
+  HandHelping,
+  UsersRound,
+  UserStar,
+  Users,
+  PiggyBank,
+  UserRoundX,
+  Banknote,
+  Landmark,
+  Coins,
+  ReceiptText,
+  Fan,
+  UserCog,
+  Scroll,
+  ChartColumn,
+  SlidersHorizontal,
+  Clipboard,
+  BadgePercent,
+} from "lucide-react";
 import { Work_Sans } from "next/font/google";
 import Link from "next/link";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-})
+});
 export default function Home() {
   return (
     <>
@@ -27,61 +49,185 @@ export default function Home() {
               <li>
                 <Link className={styles.customersNav} href="#">
                   <span>
-                    <Users size={16} />
+                    <UsersRound size={20} />
                   </span>{" "}
                   Users
                 </Link>
               </li>
               <li>
                 {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Users size={20} />
+                  </span>
                   Guarantors
                 </Link>
               </li>
               <li>
                 {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <HandHelping size={20} />
+                  </span>
                   Loans
                 </Link>
               </li>
               <li>
                 {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <HandshakeIcon size={20} />
+                  </span>
                   Decision Models
                 </Link>
               </li>
               <li>
                 {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <PiggyBank size={20} />
+                  </span>
                   Savings
                 </Link>
               </li>
               <li>
                 {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <HandHelping size={20} />
+                  </span>
                   Loan Requests
                 </Link>
               </li>
               <li>
-                {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <UserStar size={20} />
+                  </span>
                   Whitelist
                 </Link>
               </li>
               <li>
-                {" "}
-                <span></span>
                 <Link className={styles.customersNav} href="#">
+                  <span>
+                    <UserRoundX size={16} />
+                  </span>
                   Karma
                 </Link>
               </li>
             </ul>
+
+            {/* BUSINESSES SECTION */}
+            <h4>Businesses</h4>
+            <ul>
+              <li>
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Briefcase size={20} />
+                  </span>{" "}
+                  Organizations
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <HandHelping size={20} />
+                  </span>
+                  Loan Products
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Landmark size={20} />
+                  </span>
+                  Savings Products
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Coins size={20} />
+                  </span>
+                  Fees and Charges
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <ReceiptText size={20} />
+                  </span>
+                  Transactions
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Fan size={20} />
+                  </span>
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <UserCog size={20} />
+                  </span>
+                  Services Account
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Scroll size={16} />
+                  </span>
+                  Settlements
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <ChartColumn size={16} />
+                  </span>
+                  Reports
+                </Link>
+              </li>
+            </ul>
+
+            <h4>Settings</h4>
+            <ul>
+              <li>
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <SlidersHorizontal size={20} />
+                  </span>{" "}
+                  Preferences
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <BadgePercent size={20} />
+                  </span>
+                  Fees and Pricing
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className={styles.customersNav} href="#">
+                  <span>
+                    <Clipboard size={20} />
+                  </span>
+                  Audit Log
+                </Link>
+              </li>
+              </ul>
           </div>
         </div>
         <div className={styles.content}></div>
